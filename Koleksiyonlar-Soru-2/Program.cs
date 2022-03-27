6fb0225695fb19f79551a05460c10c097ee1f76d
Tugrul Okumus <cto1997@gmail.com>
2022-03-27 14:55:37 +0300

    First commit

---
+++ Koleksiyonlar-Soru-2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace koleksiyonlar_soru_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[20];
+
+            for (int i = 0; i < 20; i++)
+                arr[i] = int.Parse(Console.ReadLine()!);
+
+            Array.Sort(arr);
+
+            int[] arr_min = new int[3];
+            int[] arr_max = new int[3];
+
+            for(int i = 0; i < 3; i++)
+            {
+                arr_min[i] = arr[(arr.Count()-1) - i];
+                arr_max[i] = arr[i];
+            }
+
+            Console.WriteLine("Average of minimum 3 numbers of array: {0}", arr_min.Average());
+            Console.WriteLine("Average of maximum 3 numbers of array: {0}", arr_max.Average());
+            Console.WriteLine("Sum of these average values {0}", arr_min.Average() + arr_max.Average());
+        }
+    }
+}
---
+++ Koleksiyonlar-Soru-3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace koleksiyonlar_soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            string sentence = Console.ReadLine()!;
+            char[] sentence_arr = sentence.ToCharArray();
+
+            char[] sesli = {'A', 'E', 'I', 'İ', 'O', 'Ö', 'U', 'Ü', 'a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü'};
+            List<char> sesli_list = new List<char>();
+
+            foreach(char item in sentence_arr)
+            {
+                if(sesli.Contains(item))
+                {
+                    sesli_list.Add(item);
+                    Console.Write(item + " ");
+                }
+            }
+
+        
+        }
+    }
+}
